633713ebbc04497532af07d7bbfa0c6fa5f7c79f
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-08-15 10:30:39 +0900

    失敗するパターンがわかったので追加

--- app/ikutsu/tests.py
+++ app/ikutsu/tests.py
@@ -3,15 +3,16 @@ import pytest
 from .views import calc_age
 
 @pytest.mark.parametrize(
-    "birth_date_year, birth_date_month, birth_date_day, expected",
+    "birth_date_year, birth_date_month, birth_date_day, move_to_date, expected",
     [
-        (1993, 2, 20, 30),
-        (1993, 2, 21, 31),
+        (1993, 2, 20, '2024-02-21', 30),
+        (1993, 2, 21, '2024-02-21', 31),
+        (1993, 2, 21, '2024-08-15', 31),
     ]
 )
-def test_calc_age(freezer, birth_date_year, birth_date_month, birth_date_day, expected):
+def test_calc_age(freezer, birth_date_year, birth_date_month, birth_date_day, move_to_date, expected):
     # arrange
-    freezer.move_to("2024-02-21")
+    freezer.move_to(move_to_date)
 
     # act
     actual = calc_age(birth_date_year, birth_date_month, birth_date_day)
