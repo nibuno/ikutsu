87092cb29002588701a5e2a2d2a6b38a2f5449d7
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-08-15 09:40:43 +0900

    UT追加

--- app/ikutsu/tests.py
+++ app/ikutsu/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+from .views import calc_age
 
-# Create your tests here.
+def test_calc_age(freezer):
+    freezer.move_to("2024-02-21")
+    assert calc_age(birth_date_year=1993, birth_date_month=2, birth_date_day=20) == 30
+    assert calc_age(birth_date_year=1993, birth_date_month=2, birth_date_day=21) == 31
