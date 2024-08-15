96489de002f77f08cd9f9e1e54e97b866e8e48d1
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-08-15 10:56:21 +0900

    ロジックとUT修正

--- app/ikutsu/tests.py
+++ app/ikutsu/tests.py
@@ -5,7 +5,7 @@ from .views import calc_age
 @pytest.mark.parametrize(
     "birth_date_year, birth_date_month, birth_date_day, move_to_date, expected",
     [
-        (1993, 2, 20, '2024-02-21', 30),
+        (1993, 2, 21, '2024-02-20', 30),
         (1993, 2, 21, '2024-02-21', 31),
         (1993, 2, 21, '2024-08-15', 31),
     ]
--- app/ikutsu/views.py
+++ app/ikutsu/views.py
@@ -40,7 +40,7 @@ def calc_age(birth_date_year: int, birth_date_month: int, birth_date_day: int) -
     age = today.year - birth_date.year
 
     # 誕生日がまだ来ていない場合、年齢を1つ減らす
-    if (today.month, today.day) > (birth_date.month, birth_date.day):
+    if (today.month, today.day) < (birth_date.month, birth_date.day):
         age -= 1
 
     return age
