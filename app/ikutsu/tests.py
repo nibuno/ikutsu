0fd363a52bf84194187bf64ad42021162c1f5b32
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-08-15 10:28:22 +0900

    parametrizeを利用

--- app/ikutsu/tests.py
+++ app/ikutsu/tests.py
@@ -1,7 +1,21 @@
+import pytest
+
 from .views import calc_age
 
-def test_calc_age(freezer):
-    # FIXME: アサーションルーレットになっているので修正
+@pytest.mark.parametrize(
+    "birth_date_year, birth_date_month, birth_date_day, expected",
+    [
+        (1993, 2, 20, 30),
+        (1993, 2, 21, 31),
+    ]
+)
+def test_calc_age(freezer, birth_date_year, birth_date_month, birth_date_day, expected):
+    # arrange
     freezer.move_to("2024-02-21")
-    assert calc_age(birth_date_year=1993, birth_date_month=2, birth_date_day=20) == 30
-    assert calc_age(birth_date_year=1993, birth_date_month=2, birth_date_day=21) == 31
+
+    # act
+    actual = calc_age(birth_date_year, birth_date_month, birth_date_day)
+    expected = expected
+
+    # assert
+    assert actual == expected
